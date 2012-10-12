73c2dee9db6f2c89cd91bc296933dc5c0c0c290d
Mike Samuel <mikesamuel@gmail.com>
2012-10-12 00:28:41 -0400

    uncommented tests, added copyright headers, and commented

--- src/main/com/google/json/EvalMinifier.java
+++ src/main/com/google/json/EvalMinifier.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.json;
 
 import java.util.ArrayList;
@@ -7,6 +21,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Given a string of valid JSON that is going to be parsed via JavaScript's
+ * {@code eval} builtin, tries to reduce the number of bytes sent over
+ * the wire by turning it into a JavaScript expression that pools constants.
+ */
 public final class EvalMinifier {
 
   public static String minify(String json) {
--- src/tests/com/google/json/EvalMinifierTest.java
+++ src/tests/com/google/json/EvalMinifierTest.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.json;
 
 import junit.framework.TestCase;
@@ -15,7 +29,7 @@ public final class EvalMinifierTest extends TestCase {
 
   @Test
   public final void testMinify() {
-/*    assertMinified("null", "null");
+    assertMinified("null", "null");
     assertMinified("[null,null]", "[null,null]");
     assertMinified("[null,null,null,null]", "[null,null,null,null]");
     assertMinified(
@@ -28,7 +42,7 @@ public final class EvalMinifierTest extends TestCase {
         "(function(a){"
         + "return[a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a]}(null))",
         "[null,null,null,null,null,null,null,null,null,null,null,null,null, "
-        + "null,null,null,null,null,null,null,null,null,null,null,null,null]");*/
+        + "null,null,null,null,null,null,null,null,null,null,null,null,null]");
     assertMinified(
         "(function(a,b){return"
         + "[a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,b,a,\"z\"]"
